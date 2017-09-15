435ce812d30f36060e52106bd935ad4a8fff33a6
mlapeter <mlapeter@gmail.com>
2017-09-15 14:19:36 -0700

    basic search filter working along with pull to refresh and load more

---
+++ app/components/GamingSessionsList/FilterModal.js
@@ -0,0 +1,133 @@
+import React, { Component, PropTypes } from "react";
+import { Button, StyleSheet, Text, View, Picker, Modal } from "react-native";
+import { colors, fontSizes } from "../../styles";
+import { FontAwesome } from "@expo/vector-icons";
+import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
+
+export default class FilterModal extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      modalVisible: false,
+      platform: this.props.platform,
+      gameId: this.props.gameId,
+      activities: [],
+      activity: this.props.activity
+    };
+  }
+
+  componentDidMount() {
+    this.updateGameActivities(this.state.gameId);
+  }
+
+  updateGameActivities(gameId) {
+    console.log("Updating Game Activities");
+    this.setState({ gameId: gameId }, () => {
+      for (var index = 0; index < this.props.gamesData.length; index++) {
+        var game = this.props.gamesData[index];
+        if (game.id == this.state.gameId) {
+          this.setState({
+            activities: game.activities
+          });
+        }
+      }
+    });
+  }
+
+  setModalVisible(visible) {
+    console.log("Modal Activity: " + this.state.activity);
+    this.setState({
+      modalVisible: visible
+    });
+  }
+
+  render() {
+    if (this.props.loading) {
+      return null;
+    }
+
+    return (
+      <View>
+        <Modal
+          animationType={"slide"}
+          transparent={false}
+          visible={this.state.modalVisible}
+          onRequestClose={() => {
+            alert("Modal has been closed using android back button.");
+          }}
+        >
+          <View style={styles.modalContainer}>
+            <Picker
+              style={styles.pickerStyle}
+              selectedValue={this.state.platform}
+              onValueChange={platform => this.setState({ platform: platform })}
+            >
+              <Picker.Item label="Xbox One" value="xbox-one" />
+              <Picker.Item label="PS4" value="ps4" />
+            </Picker>
+            <Picker
+              style={styles.pickerStyle}
+              selectedValue={this.state.gameId.toString()}
+              onValueChange={gameId => this.updateGameActivities(gameId)}
+            >
+              <Picker.Item label="Destiny" value="1" />
+              <Picker.Item label="Destiny 2" value="23" />
+            </Picker>
+
+            <Picker
+              style={styles.pickerStyle}
+              selectedValue={this.state.activity}
+              onValueChange={activity => this.setState({ activity: activity })}
+            >
+              <Picker.Item label="Any" value="" />
+              {this.state.activities.map(activity =>
+                <Picker.Item
+                  key={activity}
+                  label={activity.toString()}
+                  value={activity}
+                />
+              )}
+            </Picker>
+            <View style={styles.modalButtonStyle}>
+              <Button
+                onPress={() => {
+                  this.setModalVisible(!this.state.modalVisible);
+                  this.props.updateFilter(
+                    this.state.platform,
+                    this.state.gameId,
+                    this.state.activity
+                  );
+                }}
+                title="Search"
+              />
+            </View>
+          </View>
+        </Modal>
+
+        <Button
+          onPress={() => {
+            this.setModalVisible(true);
+          }}
+          title="Show Filters"
+        />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  modalContainer: {
+    padding: 10,
+    marginBottom: 10,
+    justifyContent: "space-around",
+    backgroundColor: colors.white
+  },
+  pickerStyle: {
+    padding: 10,
+    marginBottom: 20
+  },
+  modalButtonStyle: {
+    padding: 20,
+    margin: 20
+  }
+});
--- app/components/GamingSessionsList/GamingSessionsList.js
+++ app/components/GamingSessionsList/GamingSessionsList.js
@@ -1,77 +1,127 @@
 import React, { Component, PropTypes, PureComponent } from "react";
 import {
   ActivityIndicator,
-  Alert,
-  Button,
   FlatList,
-  Image,
-  ListView,
-  TouchableHighlight,
-  RefreshControl,
+  Modal,
   StyleSheet,
   Text,
-  TextInput,
   View
 } from "react-native";
 import { StackNavigator } from "react-navigation";
-
 import PreSplash from "../../components/PreSplash/PreSplash";
 import { colors, fontSizes } from "../../styles";
-import Moment from "../../../node_modules/react-moment";
+import GamingSession from "./GamingSession";
+import FilterModal from "../../components/GamingSessionsList/FilterModal";
 import { FontAwesome } from "@expo/vector-icons";
 import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
-import GamingSession from "./GamingSession";
-
-Moment.globalFormat = "h:mm";
-Moment.globalLocale = "en";
 
 export default class GamingSessionsList extends React.PureComponent {
   constructor(props) {
     super(props);
     this.state = {
       isLoading: true,
+      refreshing: false,
       data: [],
+      moreDataAvailable: true,
+      gamesData: null,
       page: 1,
+      gameId: 23,
+      activity: "",
+      platform: "ps4",
+      notFull: 1,
       error: null,
-      refreshing: false
+      modalVisible: false
     };
-  }
-
-  onRefresh() {
-    this.setState({ refreshing: true });
-    this.fetchData().then(() => {
-      this.setState({ refreshing: false });
-    });
+    this.updateFilter = this.updateFilter.bind(this);
   }
 
   componentDidMount() {
-    this.fetchData();
+    this.fetchGamesData();
+    this.updateFilter(
+      this.state.platform,
+      this.state.gameId,
+      this.state.activity
+    );
   }
 
-  fetchData() {
-    console.log("Fetching Games Page: " + this.state.page);
-    return fetch(
-      "https://the100.io/api/v2/gaming_sessions?page=" + this.state.page
-    )
+  fetchGamesData() {
+    return fetch("https://www.the100.io/api/v1/games")
       .then(response => response.json())
       .then(responseJson => {
         this.setState({
-          isLoading: false,
-          refreshing: false,
-          data:
-            this.state.page === 1
-              ? responseJson
-              : [...this.state.data, ...responseJson],
-          error: responseJson.error || null
+          gamesData: responseJson
         });
       })
       .catch(error => {
-        this.setState({ error, loading: false, refreshing: false });
-        console.log("Error Fetching Games");
+        console.log("Error Fetching Games Data");
+      });
+  }
+
+  updateFilter(platform, gameId, activity) {
+    this.setState(
+      {
+        gameId: gameId,
+        activity: activity,
+        platform: platform,
+        data: [],
+        moreDataAvailable: true,
+        page: 1
+      },
+      () => {
+        this.fetchData();
+      }
+    );
+  }
+
+  searchUrl() {
+    return encodeURI(
+      "https://the100.io/api/v2/gaming_sessions?page=" +
+        this.state.page +
+        "&q[game_id_eq]=" +
+        this.state.gameId +
+        "&q[platform_cont]=" +
+        this.state.platform +
+        "&q[category_cont]=" +
+        this.state.activity +
+        "&q[with_available_slots]=" +
+        this.state.notFull
+    );
+  }
+
+  fetchData() {
+    console.log(this.searchUrl());
+    return fetch(this.searchUrl())
+      .then(response => response.json())
+      .then(responseJson => {
+        console.log("Games Fetched");
+        if (responseJson.length === 0) {
+          console.log("No More Data");
+          this.setState({
+            isLoading: false,
+            refreshing: false,
+            moreDataAvailable: false
+          });
+        } else {
+          console.log("Games Found");
+          this.setState({
+            isLoading: false,
+            refreshing: false,
+            data:
+              this.state.page === 1
+                ? responseJson
+                : [...this.state.data, ...responseJson],
+            error: responseJson.error || null
+          });
+        }
+      })
+      .catch(error => {
+        this.setState({ error, isloading: false, refreshing: false });
+        console.log("Error Fetching Gaming Sessions");
       });
   }
 
   handleRefresh = () => {
+    console.log("handleRefresh Triggered");
     this.setState(
       {
         page: 1,
@@ -84,19 +134,29 @@ export default class GamingSessionsList extends React.PureComponent {
   };
 
   handleLoadMore = () => {
-    this.setState(
-      {
-        page: this.state.page + 1,
-        refreshing: true
-      },
-      () => {
-        this.fetchData();
-      }
-    );
+    console.log("handleLoadMore Triggered");
+    if (this.state.moreDataAvailable === true) {
+      this.setState(
+        {
+          page: this.state.page + 1,
+          refreshing: true
+        },
+        () => {
+          this.fetchData();
+        }
+      );
+    }
   };
 
+  setModalVisible(visible) {
+    this.setState({ modalVisible: visible });
+  }
+
   renderFooter = () => {
-    if (!this.state.refreshing) {
+    if (!this.state.moreDataAvailable) {
+      console.log(
+        "In Footer moreDataAvailable: " + this.state.moreDataAvailable
+      );
       return (
         <View style={styles.alertView}>
           <MaterialCommunityIcons
@@ -106,12 +166,13 @@ export default class GamingSessionsList extends React.PureComponent {
           />
         </View>
       );
+    } else {
+      return (
+        <View style={{ paddingVertical: 20 }}>
+          <ActivityIndicator />
+        </View>
+      );
     }
-    return (
-      <View style={{ paddingVertical: 20 }}>
-        <ActivityIndicator />
-      </View>
-    );
   };
 
   render() {
@@ -125,13 +186,24 @@ export default class GamingSessionsList extends React.PureComponent {
 
     return (
       <View style={styles.container}>
+        <FilterModal
+          updateFilter={this.updateFilter}
+          gameId={this.state.gameId}
+          activity={this.state.activity}
+          platform={this.state.platform}
+          notFull={this.state.notFull}
+          gamesData={this.state.gamesData}
+        />
+
         <FlatList
           data={this.state.data}
           renderItem={({ item }) =>
             <GamingSession data={item} navigation={this.props.navigation} />}
           ListHeaderComponent={this.renderEmpty}
           ListFooterComponent={this.renderFooter}
-          keyExtractor={item => item.id}
+          // Getting errors using game id
+          // keyExtractor={item => item.id}
+          keyExtractor={(item, index) => index}
           onRefresh={this.handleRefresh}
           refreshing={this.state.refreshing}
           onEndReached={this.handleLoadMore}
@@ -158,12 +230,6 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     justifyContent: "center"
   },
-  alertText: {
-    fontSize: fontSizes.large,
-    fontWeight: "bold",
-    paddingTop: 5
-  },
-
   loading: {
     alignItems: "center",
     justifyContent: "center",
