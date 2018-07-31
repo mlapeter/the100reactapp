25f425579bdecf4b90ae619499105f73a20b86ea
mlapeter <mlapeter@gmail.com>
2018-07-31 16:41:27 -0700

    add reactotron saga

--- app/config/reactotron.js
+++ app/config/reactotron.js
@@ -1,7 +1,9 @@
 import Reactotron from "reactotron-react-native";
+import sagaPlugin from "reactotron-redux-saga";
 
 import host from "./host";
 
 Reactotron.configure({ host }) // controls connection & communication settings
-  .useReactNative() // add all built-in react native plugins
+  .useReactNative()
+  .use(sagaPlugin()) // add all built-in react native plugins
   .connect(); // let's connect!
--- app/config/store.js
+++ app/config/store.js
@@ -1,11 +1,16 @@
+require("./reactotron");
+
 import { createStore, applyMiddleware } from "redux";
 import logger from "redux-logger";
 import createSagaMiddleware from "redux-saga";
+import Reactotron from "reactotron-react-native";
 
 import reducers from "../reducers";
 import rootSaga from "./sagas";
 
-const sagaMiddleware = createSagaMiddleware();
+const sagaMonitor = Reactotron.createSagaMonitor();
+
+const sagaMiddleware = createSagaMiddleware({ sagaMonitor });
 const middleware = [sagaMiddleware];
 
 if (process.env.NODE_ENV === "development") {
