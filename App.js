b349ad1b36633e90ffa9c1e90b031824fd998e93
mlapeter <mlapeter@gmail.com>
2018-08-08 06:39:58 -0700

    fix android Module AppRegistry error by removing reactotron saga

--- App.js
+++ App.js
@@ -1,10 +1,10 @@
 import App from "./app/index";
 
-// if (__DEV__) {
-//   require("./app/config/reactotron");
-//
-//   const Reactotron = require("reactotron-react-native").default;
-//   Reactotron.log("Hello world");
-// }
+if (__DEV__) {
+  require("./app/config/reactotron");
+
+  const Reactotron = require("reactotron-react-native").default;
+  Reactotron.log("Hello world");
+}
 
 export default App;
